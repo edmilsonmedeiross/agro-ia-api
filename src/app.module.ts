5e52368e479d94b196d7f980283e7793ab3313c4
Eddie <edmilson_jb@hotmail.com>
2024-04-24 17:29:38 -0300

    feat/ -> implementa request basica openai

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionsModule } from './questions/questions.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [QuestionsModule],
+  imports: [QuestionsModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateQuestionDto } from './dto/create-question.dto';
+import OpenAI from 'openai';
 
 @Injectable()
 export class QuestionsService {
-  create(createQuestionDto: CreateQuestionDto) {
-    return 'This action adds a new question';
+  async create(createQuestionDto: CreateQuestionDto) {
+    console.log(process.env.OPENAI_API_KEY);
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+      baseURL: process.env.OPENAI_BASE_URL,
+    });
+    try {
+      const response = await openai.chat.completions.create({
+        messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+        model: 'gpt-3.5-turbo',
+      });
+
+      console.log('response', response);
+
+      return 'This action adds a new question';
+    } catch (error) {
+      console.log('error', error);
+    }
   }
 }
