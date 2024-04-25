d1acf77dc47e0e822b38393587bd85da0c53b6f5
Eddie <edmilson_jb@hotmail.com>
2024-04-25 16:44:03 -0300

    feat/ -> finaliza integração com gpt

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [QuestionsController],
   providers: [QuestionsService],
 })
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,23 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import OpenAI from 'openai';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class QuestionsService {
-  async create(createQuestionDto: CreateQuestionDto) {
+  constructor(private readonly httpService: HttpService) {}
+
+  async createGpt(createQuestionDto: CreateQuestionDto) {
+    if (
+      !createQuestionDto ||
+      !createQuestionDto.messages ||
+      createQuestionDto.messages.length === 0
+    ) {
+      return 'Please provide at least one message';
+    }
+
+    if (
+      createQuestionDto.messages.some(
+        (message) => !message.content || message.content.length < 3,
+      )
+    ) {
+      return 'Please provide a message with at least 3 characters';
+    }
+
     const openai = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY,
       baseURL: process.env.OPENAI_BASE_URL,
     });
     try {
       const response = await openai.chat.completions.create({
-        messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+        messages: createQuestionDto.messages.map((message) => ({
+          role: 'user',
+          content: message.content,
+        })),
         model: 'gpt-3.5-turbo',
       });
 
-      console.log('response', response);
+      const messages = response.choices.map((choice, index) => ({
+        option: index + 1,
+        message: choice.message.content,
+      }));
 
-      return 'This action adds a new question';
+      return messages || [];
     } catch (error) {
       console.log('error', error);
     }
