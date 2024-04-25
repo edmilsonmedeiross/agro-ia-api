132b21608bd7973dad262ecfc14adbef21d7fb2d
Eddie <edmilson_jb@hotmail.com>
2024-04-25 16:43:38 -0300

    feat/ -> configura validação e swagger

--- src/questions/dto/create-question.dto.ts
+++ src/questions/dto/create-question.dto.ts
@@ -1,1 +1,16 @@
-export class CreateQuestionDto {}
+import { IsString, IsArray, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+class MessageDto {
+  @IsString({
+    message: 'Content must be a string',
+  })
+  content: string;
+}
+
+export class CreateQuestionDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => MessageDto)
+  messages: MessageDto[];
+}
--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,12 +1,47 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { CreateQuestionDto } from './dto/create-question.dto';
+import { ApiBody, ApiResponse } from '@nestjs/swagger';
 @Controller('questions')
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
-
-  @Post()
-  create(@Body() createQuestionDto: CreateQuestionDto) {
-    return this.questionsService.create(createQuestionDto);
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        messages: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              content: {
+                type: 'string',
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    schema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          option: {
+            type: 'number',
+          },
+          message: {
+            type: 'string',
+          },
+        },
+      },
+    },
+  })
+  @Post('gpt')
+  createGpt(@Body() createQuestionDto: CreateQuestionDto) {
+    return this.questionsService.createGpt(createQuestionDto);
   }
 }
